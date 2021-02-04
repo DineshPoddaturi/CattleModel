4430d304c33e38336c54bb31a0aca76ab2eafe0c
Dinesh R Poddaturi <dinesh@iastate.edu>
2021-02-04 13:39:00 -0600

    New code added for the increased costs

--- test.R
+++ test.R
@@ -1058,7 +1058,9 @@ parameter_s <- parameters_new %>% ggplot(aes(x=Year)) + geom_line(aes(y=s_tilde)
 # We have prices per head. Need to convert them to price per pound. So basically this is same for steers, heifers, and cows.
 prices_costs_new <- prices_costs
 
-# We use tagging costs from CostEstimates_SAV.Rmd. 
+# We use tagging costs from CostEstimates_SAV.Rmd. These costs are sum of all the costs (which are weighted average of costs for each capacity).
+# See CostEstimates_SAV.Rmd for details.
+
 taggingCosts <- round(total_Costs,3)
 
 aggCosts <- (Stock_temp[,2] * taggingCosts) %>% as.data.frame()
@@ -1097,7 +1099,140 @@ costs_sl_cl <- merge(costs_cl_9years, costs_sl_2years) %>% select(Year, cost_Lb_
 # Are we going to keep the observed supply and demand as it is?
 # Keep the same demand as this year but change the supply?
 # We know the share depends on ps and pc, so if it changes the sl & cl supply changes as well. Think more about these scenarios
-# 
+
+
+# I am building the added costs dataframe
+# Change the costs from 2009. I take the year from the paper
+cost_price_hat <- prices_predict %>% filter(ps_hat>0)
+
+
+cost_price_addedCosts <- merge(cost_price_hat,costs_sl_cl) %>% select(Year, ps_hat, pc_hat, hc_hat, cost_both)
+
+ccc <- cost_price_addedCosts %>% filter(Year>=2009) %>% mutate(hc_hat = hc_hat + cost_both) %>% select (-cost_both)
+
+cost_price_addedCosts <- left_join(cost_price_addedCosts, ccc, by = "Year") %>% 
+  mutate(ps_hat = ps_hat.x, pc_hat = pc_hat.x, hc_hat = ifelse(is.na(hc_hat.y), hc_hat.x, hc_hat.y)) %>% 
+  select(Year, ps_hat, pc_hat, hc_hat)
+
+
+
+
+
+# I will also add the costs to the observed prices and costs. I will try the predictions with the observed first and see how things turn out.
+cost_price_obs <- merge(prices_costs,costs_sl_cl) %>% select(Year, ps, pc, hc, cost_both)
+
+costs_TBAdded <- cost_price_obs %>% filter(Year>=2009) %>% mutate(hc = hc + cost_both) %>% select (-cost_both)
+
+cost_price_addedCosts_obs <- left_join(cost_price_obs, costs_TBAdded, by = "Year") %>% 
+  mutate(ps = ps.x, pc = pc.x, hc = ifelse(is.na(hc.y), hc.x, hc.y)) %>% 
+  select(Year, ps, pc, hc)
+
+
+
+# Now I have to include these new price and cost estimates to predict the prices.
+
+## Here we use the slaughter animals numbers
+Stock_temp <- Stock%>% filter(Year>1994 & Year<=2017)
+imports_temp <- imports %>% filter(Year>1994 & Year<=2017)
+exports_temp <- exports %>% filter(Year>1994 & Year<=2017)
+
+predict_df <- cbind(Stock_temp$Year, Stock_temp$K, Stock_temp$k3 , imports_temp$Imports, exports_temp$Exports,
+                    dressedWeights_sl_cl %>% filter(Year>1994)%>% select(Slaughter_avg),
+                    prices_costs%>%filter(Year>1994)%>% select(ps), prices_costs%>%filter(Year>1994)%>% select(pc),
+                    prices_costs%>%filter(Year>1994)%>% select(hc), supp_sl %>% filter(Year>=1995) %>% select(Bill_meatLb_sl), 
+                    supp_cl %>% filter(Year>=1995) %>% select(Bill_meatLb_cl),
+                    totalDisappearedNew %>% filter(Year>=1995) %>% select(total_meat_bill)) %>% as.data.frame()
+names(predict_df) <- c("Year", "K", "k3", "imports", "exports", "dressedWeight", "ps", "pc", "hc", "sl", "cl", "Dissappear")
+
+
+demand_predict <- data.frame(Year = predict_df$Year+1, demand_est = numeric(nrow(predict_df)), sl_est = numeric(nrow(predict_df)), cl_est = numeric(nrow(predict_df)))
+
+# demand_predict <- data.frame(Year = predict_df$Year+1, demand_est = numeric(nrow(predict_df)))
+
+prices_predict <- data.frame(Year = predict_df$Year+1, ps_hat = numeric(nrow(predict_df)), pc_hat = numeric(nrow(predict_df)), hc_hat = numeric(nrow(predict_df)))
+
+parameters <- data.frame(Year = predict_df$Year+1, mu_tilde = numeric(nrow(predict_df)), s_tilde = numeric(nrow(predict_df)))
+
+for(i in 1:(nrow(predict_df)-2)){
+  
+  # i <- 2
+  K_t <- predict_df$K[i]
+  k3_t2 <- predict_df$k3[i+2]
+  # imports_t <- predict_df$imports[i]
+  
+  if(i<=1){
+    ps_t <- predict_df$ps[i]
+    pc_t <- predict_df$pc[i]
+    hc_t <- predict_df$hc[i]
+    sl <- predict_df$sl[i]
+    cl <- predict_df$cl[i]
+    demand <- predict_df$Dissappear[i]
+  }
+  
+  ps_t <- predict_df$ps[i]
+  pc_t <- predict_df$pc[i]
+  hc_t <- predict_df$hc[i]
+  dressed_t <- predict_df$dressedWeight[i]
+  sl <- predict_df$sl[i]
+  cl <- predict_df$cl[i]
+  demand <- predict_df$Dissappear[i]
+  adj <- demand/(sl+cl)
+  
+  imports_t <- predict_df$imports[i]
+  exports_t <- predict_df$exports[i]
+  
+  
+  slShare_t <- (exp((muTilde - ((ps_t - pc_t))/phi)/sTilde))
+  
+  demand_t1_hat <- (g * K_t - k3_t2 + imports_t - exports_t) * (dressed_t/1000000000) * ((1+slShare_t)/slShare_t)
+  
+  sl_t1_hat <- (demand_t1_hat * ((slShare_t)/(1 + slShare_t))) * adj
+  cl_t1_hat <- (demand_t1_hat * 1/(1+slShare_t)) * adj
+  
+  params_t1 <- mu_s_tildes(sl=sl_t1_hat, cl=cl_t1_hat, ps = ps_t, pc = pc_t, thetas = c(1,1))
+  parameters$mu_tilde[i] <- params_t1[1]
+  parameters$s_tilde[i] <- params_t1[2]
+  
+  
+  # slShare_t <- (exp((params_t1[1] - ((ps_t - pc_t))/phi)/params_t1[2]))
+  # 
+  # demand_t1_hat <- (g * K_t - k3_t2 + imports_t - exports_t) * (dressed_t/1000000000) * ((1+slShare_t)/slShare_t)
+  # 
+  # sl_t1_hat <- (demand_t1_hat * ((slShare_t)/(1 + slShare_t))) * adj
+  # cl_t1_hat <- (demand_t1_hat * 1/(1+slShare_t)) * adj
+  
+  
+  
+  p <- c(ps_t, pc_t, hc_t)
+  sl <- sl_t1_hat
+  cl <- cl_t1_hat
+  A <- demand_t1_hat
+  mu_Tilde <- params_t1[1]
+  s_Tilde <- params_t1[2]
+  
+  est_bb <- BBoptim(par=p, fn = sysEqs_solve)$par
+  ps_hat_t1 <- est_bb[1]
+  pc_hat_t1 <- est_bb[2]
+  hc_hat_t1 <- est_bb[3]
+  
+  prices_predict$ps_hat[i] <- ps_hat_t1
+  prices_predict$pc_hat[i] <- pc_hat_t1
+  prices_predict$hc_hat[i] <- hc_hat_t1
+  demand_predict$demand_est[i] <- demand_t1_hat
+  demand_predict$sl_est[i] <- sl_t1_hat
+  demand_predict$cl_est[i] <- cl_t1_hat
+  
+  # ps_t <- ps_hat_t1
+  # pc_t <- pc_hat_t1
+  # hc_t <- hc_hat_t1
+  # demand <- A
+}
+
+
+
+
+
+
 
 
 
