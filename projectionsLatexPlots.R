ce73219c7454627a6baa7cf1618db5346e127598
Dinesh R Poddaturi <dinesh@iastate.edu>
2022-03-01 16:16:21 -0600

    Plots for the model projections are added

--- projectionsLatexPlots.R
+++ projectionsLatexPlots.R
@@ -6,21 +6,21 @@ require(tikzDevice)
 # 
 # 
 
-projPS_plots <- EQestObsPS_Medians_proj
+projPS_plots <- EQestObsPS_Medians_proj %>% filter(Year > 2013)
 
-tikz(file="projectionsLatexPlots/ProjectedFedCattlePricePlot.tex", width=6, height=3)
+tikz(file="projectionsLatexPlots/ProjectedFedCattlePricePlot.tex", width=6.2, height=3.5)
 
 projectedFedPrice_plot <- projPS_plots %>% ggplot(aes(x=Year))+
-  geom_line(aes(y=psMedian, color="Fitted")) + geom_point(aes(y = psMedian, color = "Fitted")) + 
-  geom_line(aes(y=ps, color = "Observed")) + geom_point(aes(y=ps, color = "Observed")) + 
+  geom_line(aes(y=psMedian, color="Fitted")) + geom_point(aes(y = psMedian, color = "Fitted")) +
   geom_line(aes(y=Ps_lo, color="Lower bound")) + geom_point(aes(y=Ps_lo, color="Lower bound")) + 
   geom_line(aes(y=Ps, color="Projected")) + geom_point(aes(y=Ps, color="Projected")) + 
   geom_line(aes(y=Ps_up, color="Upper bound"))  + geom_point(aes(y=Ps_up, color="Upper bound")) +
   scale_x_continuous(name="Year", 
                      breaks=c(seq(projPS_plots$Year[1],
-                                  projPS_plots$Year[nrow(projPS_plots)]))) +
-  scale_y_continuous(name="Fed cattle Price") + 
-  theme(legend.position = c(0.1, 0.75)) + theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1))
+                                  projPS_plots$Year[nrow(projPS_plots)]))) + theme_classic() + 
+  theme(legend.position="bottom", legend.box = "horizontal") +
+  theme(legend.title=element_blank()) +
+  scale_y_continuous(name="Fed Cattle Price") + theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1))
 
 print(projectedFedPrice_plot)
 
@@ -28,43 +28,43 @@ dev.off()
 
 
 
-projPC_plots <- EQestObsPC_Medians_proj
+projPC_plots <- EQestObsPC_Medians_proj %>% filter(Year > 2013)
 
-tikz(file="projectionsLatexPlots/ProjectedCullCowPricePlot.tex", width=6, height=3)
+tikz(file="projectionsLatexPlots/ProjectedCullCowPricePlot.tex",  width=6.2, height=3.5)
 
 projectedCullPrice_plot <- projPC_plots %>% ggplot(aes(x=Year))+
-  geom_line(aes(y=pcMedian, color="Fitted")) + geom_point(aes(y = pcMedian, color = "Fitted")) + 
-  geom_line(aes(y=pc, color = "Observed")) + geom_point(aes(y=pc, color = "Observed")) + 
-  geom_line(aes(y=Pc_lo, color="Lower bound")) + geom_point(aes(y=Pc_lo, color="Lower bound")) + 
+  geom_line(aes(y=pcMedian, color="Fitted")) + geom_point(aes(y = pcMedian, color = "Fitted")) +  
+  geom_line(aes(y=Pc_lo, color="Upper bound")) + geom_point(aes(y=Pc_lo, color="Upper bound")) + 
   geom_line(aes(y=Pc, color="Projected")) + geom_point(aes(y=Pc, color="Projected")) + 
-  geom_line(aes(y=Pc_up, color="Upper bound"))  + geom_point(aes(y=Pc_up, color="Upper bound"))  +
+  geom_line(aes(y=Pc_up, color="Lower bound"))  + geom_point(aes(y=Pc_up, color="Lower bound"))  +
   scale_x_continuous(name="Year", 
                      breaks=c(seq(projPC_plots$Year[1],
-                                  projPC_plots$Year[nrow(projPC_plots)]))) + 
-  scale_y_continuous(name="Cull cow price") +
-  theme(legend.position = c(0.1, 0.75)) + theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1)) 
+                                  projPC_plots$Year[nrow(projPC_plots)])))  + theme_classic() + 
+  theme(legend.position="bottom", legend.box = "horizontal") +
+  theme(legend.title=element_blank()) +
+  scale_y_continuous(name="Cull Cow Price")  + theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1)) 
 
 print(projectedCullPrice_plot)
 
 dev.off()
 
+# +theme(legend.position = c(0.1, 0.75))
 
+projSL_plots <- EQestObsSL_Medians_proj %>% filter(Year > 2013)
 
-projSL_plots <- EQestObsSL_Medians_proj
-
-tikz(file="projectionsLatexPlots/ProjectedFedCattleProduction.tex", width=6, height=3)
+tikz(file="projectionsLatexPlots/ProjectedFedCattleProduction.tex",  width=6.2, height=3.5)
 
 projectedFedProduction_plot <- projSL_plots %>% ggplot(aes(x=Year))+
-  geom_line(aes(y=slMedian, color="Fitted")) + geom_point(aes(y = slMedian, color = "Fitted")) + 
-  geom_line(aes(y=SlObs, color = "Observed")) + geom_point(aes(y=SlObs, color = "Observed")) + 
+  geom_line(aes(y=slMedian, color="Fitted")) + geom_point(aes(y = slMedian, color = "Fitted")) +  
   geom_line(aes(y=Sl_lo, color="Lower bound")) + geom_point(aes(y=Sl_lo, color="Lower bound")) + 
   geom_line(aes(y=Sl, color="Projected")) + geom_point(aes(y=Sl, color="Projected")) + 
   geom_line(aes(y=Sl_up, color="Upper bound"))  + geom_point(aes(y=Sl_up, color="Upper bound"))  +
   scale_x_continuous(name="Year", 
                      breaks=c(seq(projSL_plots$Year[1],
-                                  projSL_plots$Year[nrow(projSL_plots)]))) + 
-  scale_y_continuous(name="Fed cattle production") +
-  theme(legend.position = c(0.1, 0.75)) + theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1)) 
+                                  projSL_plots$Year[nrow(projSL_plots)]))) + theme_classic() + 
+  theme(legend.position="bottom", legend.box = "horizontal") +
+  theme(legend.title=element_blank()) + 
+  scale_y_continuous(name="Fed Cattle Production") + theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1)) 
 
 print(projectedFedProduction_plot)
 
@@ -72,25 +72,77 @@ dev.off()
 
 
 
-projCL_plots <- EQestObsCL_Medians_proj
+projCL_plots <- EQestObsCL_Medians_proj %>% filter(Year > 2013)
 
-tikz(file="projectionsLatexPlots/ProjectedCullCowProduction.tex", width=6, height=3)
+tikz(file="projectionsLatexPlots/ProjectedCullCowProduction.tex",  width=6.2, height=3.5)
 
 projectedCowsProduction_plot <- projCL_plots %>% ggplot(aes(x=Year))+
   geom_line(aes(y=clMedian, color="Fitted")) + geom_point(aes(y = clMedian, color = "Fitted")) + 
-  geom_line(aes(y=ClObs, color = "Observed")) + geom_point(aes(y=ClObs, color = "Observed")) + 
   geom_line(aes(y=Cl_lo, color="Lower bound")) + geom_point(aes(y=Cl_lo, color="Lower bound")) + 
   geom_line(aes(y=Cl, color="Projected")) + geom_point(aes(y=Cl, color="Projected")) + 
   geom_line(aes(y=Cl_up, color="Upper bound"))  + geom_point(aes(y=Cl_up, color="Upper bound"))  +
   scale_x_continuous(name="Year", 
                      breaks=c(seq(projCL_plots$Year[1],
-                                  projCL_plots$Year[nrow(projCL_plots)]))) + 
-  scale_y_continuous(name="Cull cow production") +
-  theme(legend.position = c(0.1, 0.75)) + theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1)) 
+                                  projCL_plots$Year[nrow(projCL_plots)]))) + theme_classic() + 
+  theme(legend.position="bottom", legend.box = "horizontal") +
+  theme(legend.title=element_blank()) + 
+  scale_y_continuous(name="Cull Cow Production") + theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1)) 
 
 print(projectedCowsProduction_plot)
 
 dev.off()
+
+
+
+
+
+CARD_USDA_FAPRI_PS_plots <- CARD_USDA_FAPRI_PS_Proj %>% filter(Year > 2013)
+
+tikz(file="projectionsLatexPlots/CARD_USDA_FAPRI_PS.tex",  width=6.2, height=4)
+
+CARD_USDA_FAPRI_PS_plot <- CARD_USDA_FAPRI_PS_plots %>% ggplot(aes(x=Year)) + 
+  geom_line(aes(y=psMedian, color="Fitted")) + geom_point(aes(y = psMedian, color = "Fitted")) + 
+  geom_line(aes(y=Ps_lo, color="Lower bound")) + geom_point(aes(y=Ps_lo, color="Lower bound")) + 
+  geom_line(aes(y=Ps, color="Projected")) + geom_point(aes(y=Ps, color="Projected")) + 
+  geom_line(aes(y=FAPRI_Ps, color="FAPRI"))  + geom_point(aes(y=FAPRI_Ps, color="FAPRI")) + 
+  geom_line(aes(y=USDA_Ps, color="USDA"))  + geom_point(aes(y=USDA_Ps, color="USDA"))  + 
+  geom_line(aes(y=Ps_up, color="Upper bound"))  + geom_point(aes(y=Ps_up, color="Upper bound"))  + 
+  scale_x_continuous(name="Year", breaks=c(seq(CARD_USDA_FAPRI_PS_plots$Year[1],
+                                               CARD_USDA_FAPRI_PS_plots$Year[nrow(CARD_USDA_FAPRI_PS_plots)])))  +
+  theme_classic() + 
+  theme(legend.position="bottom", legend.box = "horizontal") +
+  theme(legend.title=element_blank()) + 
+  scale_y_continuous(name="Fed Cattle Price") + theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1)) 
+
+print(CARD_USDA_FAPRI_PS_plot)
+
+dev.off()
+
+
+CARD_USDA_FAPRI_TS_plots <- CARD_USDA_FAPRI_TS_Proj %>% filter(Year > 2013)
+
+tikz(file="projectionsLatexPlots/CARD_USDA_FAPRI_TS.tex",  width=6.2, height=4)
+
+CARD_USDA_FAPRI_TS_plot <- CARD_USDA_FAPRI_TS_plots %>% ggplot(aes(x=Year)) + 
+  geom_line(aes(y=tsMedian, color="Fitted")) + geom_point(aes(y=tsMedian, color="Fitted")) + 
+  geom_line(aes(y=TS_lo, color="Lower Bound")) + geom_point(aes(y=TS_lo, color="Lower Bound")) + 
+  geom_line(aes(y=TS, color="Projected")) + geom_point(aes(y=TS, color="Projected")) + 
+  geom_line(aes(y=FAPRI_TS, color="FAPRI"))  + geom_point(aes(y=FAPRI_TS, color="FAPRI")) +
+  geom_line(aes(y=USDA_TS, color="USDA"))  + geom_point(aes(y=USDA_TS, color="USDA"))  + 
+  geom_line(aes(y=TS_up, color="Upper Bound"))  + geom_point(aes(y=TS_up, color="Upper Bound"))  + 
+  scale_x_continuous(name="Year", 
+                     breaks=c(seq(CARD_USDA_FAPRI_TS_plots$Year[1],
+                                  CARD_USDA_FAPRI_TS_plots$Year[nrow(CARD_USDA_FAPRI_TS_plots)]))) +
+  theme_classic() + 
+  theme(legend.position="bottom", legend.box = "horizontal") +
+  theme(legend.title=element_blank()) + 
+  scale_y_continuous(name="Total Supply") + theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1)) 
+
+print(CARD_USDA_FAPRI_TS_plot)
+
+dev.off()
+
+
 
 
 
--- rationalExpectationSolutionPlotsTikz.R
+++ rationalExpectationSolutionPlotsTikz.R
@@ -9,7 +9,7 @@ require(tikzDevice)
 EQestObsPSNI_plots <- EQestObsPSNI  %>% select(Year, psMean, psMedian, ps) %>% 
   transmute(Year = Year, psMean = psMean * 100, psMedian = psMedian * 100, ps = ps * 100)
 
-tikz(file="rationalExpectationsLatexPlots/FedCattlePricePlot.tex", width=6, height=3)
+tikz(file="rationalExpectationsLatexPlots/FedCattlePricePlot.tex", width=6.2, height=3.5)
 
 slaughter_plot <- EQestObsPSNI_plots%>% ggplot(aes(x=Year))+geom_line(aes(y=psMean, color="Mean fitted price")) +
   geom_point(aes(y = psMean, color = "Mean fitted price")) + geom_line(aes(y=ps, color = "Observed price")) + 
@@ -17,8 +17,9 @@ slaughter_plot <- EQestObsPSNI_plots%>% ggplot(aes(x=Year))+geom_line(aes(y=psMe
   geom_point(aes(y = psMedian, color = "Median fitted price")) + theme_classic() + 
   scale_x_continuous(name="Year", 
                      breaks=c(seq(EQestObsPSNI_plots$Year[1],EQestObsPSNI_plots$Year[nrow(EQestObsPSNI_plots)]))) +
-  scale_y_continuous(name="Fed Cattle Price")+
-  theme(legend.position = c(0.2, 0.7)) + theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1))
+  scale_y_continuous(name="Fed Cattle Price")+ theme_classic() + 
+  theme(legend.position="bottom", legend.box = "horizontal") +
+  theme(legend.title=element_blank()) + theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1))
 
 print(slaughter_plot)
 
@@ -28,7 +29,7 @@ dev.off()
 EQestObsPCNI_plots <- EQestObsPCNI %>% select(Year, pcMean, pcMedian, pc) %>% 
   transmute(Year = Year, pcMean = pcMean * 100, pcMedian = pcMedian * 100, pc = pc * 100)
 
-tikz(file="rationalExpectationsLatexPlots/CullCowPricePlot.tex", width=6, height=3)
+tikz(file="rationalExpectationsLatexPlots/CullCowPricePlot.tex", width=6.2, height=3.5)
 
 cull_plot <- EQestObsPCNI_plots%>% ggplot(aes(x=Year))+geom_line(aes(y=pcMean, color="Mean fitted price")) +
   geom_point(aes(y = pcMean, color = "Mean fitted price")) + geom_line(aes(y=pc, color = "Observed price")) + 
@@ -36,8 +37,9 @@ cull_plot <- EQestObsPCNI_plots%>% ggplot(aes(x=Year))+geom_line(aes(y=pcMean, c
   geom_point(aes(y = pcMedian, color = "Median fitted price")) + theme_classic() + 
   scale_x_continuous(name="Year", 
                      breaks=c(seq(EQestObsPCNI_plots$Year[1],EQestObsPCNI_plots$Year[nrow(EQestObsPCNI_plots)]))) +
-  scale_y_continuous(name="Cull Cow Price") +
-  theme(legend.position = c(0.2, 0.7)) + theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1))
+  scale_y_continuous(name="Cull Cow Price") +theme_classic() + 
+  theme(legend.position="bottom", legend.box = "horizontal") +
+  theme(legend.title=element_blank()) + theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1))
 
 print(cull_plot)
 
